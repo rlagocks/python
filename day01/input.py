acafac4ff1218b8c7af52ac02748514e183db38f
samci <140997210+rlagocks@users.noreply.github.com>
2024-04-09 21:41:34 +0900

    Add files via upload

---
+++ day01/input.py
@@ -0,0 +1,19 @@
+# print : 글 나오도록 하는 기능
+# input : 글을 입력하는 기능
+
+# idol = input("당신의 최애 아이돌은? ")
+# print(f"당신의 최애 아이돌은 {idol}군요!")
+#
+# name = input("당신의 이름은?")
+# print(f"당신의 이름은 {name}이군요!")
+
+name = input("당신의 이름은?")
+age = input("당신의 나이는?")
+color = input("당신이 좋아하는 색은?")
+
+print(f"제 이름은 {name}, 나이는 {age}이고, 좋아하는 색은 {color}입니다!")
+
+date = input("오늘의 날짜는?")
+schedule = input("계획하신 일정은?")
+print(f"오늘은 {date}, 계획하신 일정은 {schedule}입니다!")
+
